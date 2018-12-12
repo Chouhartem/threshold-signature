e3480d15bd9217b88fa5132520629443236c8208
Fabrice Mouhartem <fabrice.mouhartem@ens-lyon.org>
2018-12-12 15:55:13 +0100

    Elements in G1 and G2

--- relic++.cpp
+++ relic++.cpp
@@ -5,17 +5,20 @@ extern "C" {
 }
 #include "relic++.h"
 
-G1 G1::operator+(G1& b)
+/******/
+/* G1 */
+/******/
+G1 G1::operator+(G1 b)
 {
   g1_t res;
-  g1_add(res, t, *b.get());
+  g1_add(res, t, b.t);
   return G1(res);
 }
 
-G1 G1::operator-(G1& b)
+G1 G1::operator-(G1 b)
 {
   g1_t res;
-  g1_sub(res, t, *b.get());
+  g1_sub(res, t, b.t);
   return G1(res);
 }
 
@@ -33,12 +36,12 @@ void G1::print()
 
 bool G1::operator==(G1 a)
 {
-  return g1_cmp(t, *a.get()) == CMP_EQ;
+  return g1_cmp(t, a.t) == CMP_EQ;
 }
 
 bool G1::operator!=(G1 a)
 {
-  return g1_cmp(t, *a.get()) == CMP_NE;
+  return g1_cmp(t, a.t) == CMP_NE;
 }
 
 void G1::get_ord(Z &n)
@@ -46,33 +49,137 @@ void G1::get_ord(Z &n)
   g1_get_ord(n.t);
 }
 
+Z G1::get_ord()
+{
+  bn_t z;
+  g1_get_ord(z);
+  return Z(z);
+}
+
+void G1::gen()
+{
+  g1_get_gen(t);
+}
+
+void G1::set_infty()
+{
+  g1_set_infty(t);
+}
+
+bool G1::valid()
+{
+  return g1_is_valid(t);
+}
+
+bool G1::is_infty()
+{
+  return g1_is_infty(t);
+}
+
 void G1::rand() {
   g1_rand(t);
 }
 
+G1 operator*(Z k, G1 g)
+{
+  G1 res;
+  g1_mul(res.t, g.t, k.t);
+  return res;
+}
+
+
+/******/
+/* G2 */
+/******/
+void G2::get_ord(Z &n)
+{
+  g2_get_ord(n.t);
+}
+
+Z G2::get_ord()
+{
+  bn_t z;
+  g2_get_ord(z);
+  return Z(z);
+}
+
+void G2::gen()
+{
+  g2_get_gen(t);
+}
+
+void G2::set_infty()
+{
+  g2_set_infty(t);
+}
+
+bool G2::is_infty()
+{
+  return g2_is_infty(t);
+}
+
+bool G2::valid()
+{
+  return g2_is_valid(t);
+}
+
+G2 G2::operator-(G2 b)
+{
+  g2_t res;
+  g2_sub(res, t, b.t);
+  return G2(res);
+}
+
+G2 G2::operator-()
+{
+  g2_t res;
+  g2_neg(res, t);
+  return G2(res);
+}
+
+bool G2::operator==(G2 a)
+{
+  return g2_cmp(t, a.t) == CMP_EQ;
+}
+
+bool G2::operator!=(G2 a)
+{
+  return g2_cmp(t, a.t) == CMP_NE;
+}
+
+G2 operator*(Z k, G2 g)
+{
+  G2 res;
+  g2_mul(res.t, g.t, k.t);
+  return res;
+}
+
+/*****/
+/* Z */
+/*****/
 Z Z::operator+(Z &b)
 {
   Z res;
-  bn_add(*res.get(), t, *b.get());
+  bn_add(res.t, t, b.t);
   return res;
 }
 
 Z Z::operator*(Z &b)
 {
   Z res;
-  bn_mul(*res.get(), t, *b.get());
+  bn_mul(res.t, t, b.t);
   return res;
 }
 Z Z::operator-(Z &b)
 {
   Z res;
-  bn_sub(*res.get(), t, *b.get());
+  bn_sub(res.t, t, b.t);
   return res;
 }
 Z Z::operator/(Z &b)
 {
   Z res;
-  bn_div(*res.get(), t, *b.get());
+  bn_div(res.t, t, b.t);
   return res;
 }
 
@@ -83,5 +190,14 @@ void Z::print()
 
 void Z::rand_mod(Z n)
 {
-  bn_rand_mod(t, *n.get());
+  bn_rand_mod(t, n.t);
+}
+
+std::ostream& operator<<(std::ostream &ss, Z z)
+{
+  char n = bn_size_str(z.t, 10);
+  char s[n+1];
+  bn_write_str(s, n, z.t, 10);
+  ss << s;
+  return ss;
 }
--- relic++.h
+++ relic++.h
@@ -1,11 +1,40 @@
+/* Copyright (C) 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
 #ifndef _H_RELICPP
 #define _H_RELICPP
+#include<ostream>
+/**
+ * @file relic++.h
+ * @brief Relic wrapper in c++
+ * @author Fabrice Mouhartem
+ * @version 0.1
+ * @date 2018-12-10
+ */
+
 
 class Z;
 class G1;
 class G2;
 class GT;
 
+/**
+ * @brief Group elements in G1
+ */
 class G1 {
   g1_t t;
   public:
@@ -20,20 +49,61 @@ class G1 {
   ~G1() {
     g1_free(t);
   }
-  const g1_t* get() {
-    return &t;
-  }
   void get_ord(Z &n);
-  G1 operator+(G1 &b);
-  G1 operator-(G1 &b);
+  Z get_ord();
+  void gen();
+  void set_infty();
+  bool is_infty();
+  bool valid();
+  G1 operator+(G1 b);
+  G1 operator-(G1 b);
   G1 operator-();
   void print();
   bool operator==(G1);
   bool operator!=(G1);
   void rand();
+
+  friend G1 operator*(Z k, G1 g);
+};
+
+/**
+ * @brief Group elements in G2
+ */
+class G2 {
+  g2_t t;
+  public:
+  G2() {
+    g2_null(t);
+    g2_new(t);
+  }
+  G2(g2_t a) {
+    g2_new(t);
+    g2_copy(t, a);
+  }
+  ~G2() {
+    g2_free(t);
+  }
+  void get_ord(Z &n);
+  Z get_ord();
+  void gen();
+  void set_infty();
+  bool is_infty();
+  bool valid();
+  G2 operator+(G2 b);
+  G2 operator-(G2 b);
+  G2 operator-();
+  void print();
+  bool operator==(G2);
+  bool operator!=(G2);
+  void rand();
+
+  friend G2 operator*(Z k, G2 g);
 };
 
 
+/**
+ * @brief Scalar elements
+ */
 class Z {
   bn_t t;
   public:
@@ -57,17 +127,22 @@ class Z {
     bn_clean(t);
     bn_free(t);
   }
-  bn_t* get()
-  {
-    return &t;
-  }
   void rand_mod(Z);
   void print();
   Z operator+(Z &b);
   Z operator*(Z &b);
   Z operator/(Z &b);
   Z operator-(Z &b);
+
   friend void G1::get_ord(Z&);
+  friend void G2::get_ord(Z&);
+  friend G1 operator*(Z k, G1 g);
+  friend G2 operator*(Z k, G2 g);
+  friend std::ostream& operator<<(std::ostream &ss, Z z);
 };
+
+G1 operator*(Z k, G1 g);
+G2 operator*(Z k, G2 g);
+std::ostream &operator<<(std::ostream &ss, Z z);
 
 #endif
