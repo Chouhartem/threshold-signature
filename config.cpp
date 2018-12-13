7e450e49a00ae83a856635aa7328556f265188a6
Fabrice Mouhartem <fabrice.mouhartem@ens-lyon.org>
2018-12-13 15:29:34 +0100

    Take care of errors

--- config.cpp
+++ config.cpp
@@ -4,12 +4,14 @@ extern "C" {
 #include <relic_bench.h>
 }
 #include "config.h"
+#include <iostream>
 
 namespace config {
   void relic_init()
   {
     if (core_init() != STS_OK) {
       core_clean();
+      std::cerr << "Error at init" << std::endl;
     }
     //conf_print();
   }
